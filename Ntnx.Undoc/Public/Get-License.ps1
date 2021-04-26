a14dc2cafc465fa7eb383b590be436e5523ea3eb
ntnx-mrrensing <51458027+ntnx-mrrensing@users.noreply.github.com>
2021-04-26 16:05:21 -0400

    Added Get-License

---
+++ Ntnx.Undoc/Public/Get-License.ps1
@@ -0,0 +1,97 @@
+function Get-License {   
+    <#
+    .SYNOPSIS
+    Dynamically Generated API Function
+    .NOTES
+    NOT FOR PRODUCTION USE - FOR DEMONSTRATION/EDUCATION PURPOSES ONLY
+    
+    The code samples provided here are intended as standalone examples.  They can be downloaded, copied and/or modified in any way you see fit.
+    
+    Please be aware that all code samples provided here are unofficial in nature, are provided as examples only, are unsupported and will need to be heavily modified before they can be used in a production environment.
+    #>
+    
+        [CmdletBinding()]
+        [OutputType()]
+    
+        param(
+            # VIP or FQDN of target AOS cluster
+            [Parameter(Mandatory=$true)]
+            [string]
+            $ComputerName,
+    
+            # Prism UI Credential to invoke call
+            [Parameter(Mandatory=$true)]
+            [PSCredential]
+            $Credential,
+    
+            # Body Parameter1
+            #[Parameter()]
+            #$BodyParam1,
+    
+            [Parameter(Mandatory=$false)]
+            [switch]
+            $SkipCertificateCheck,
+    
+            [Parameter(Mandatory=$false)]
+            [switch]
+            $ShowMetadata,
+    
+            # Port (Default is 9440)
+            [Parameter(Mandatory=$false)]
+            [int16]
+            $Port = 9440
+        )
+    
+        begin {
+            Set-StrictMode -Version Latest
+        }
+    
+        process {
+            $body = [Hashtable]::new()
+            #$body.add("kind","kind")
+            #$body.add("BodyParam1",$BodyParam1)
+    
+            $iwrArgs = @{
+                Uri = "https://$($ComputerName):$($Port)/PrismGateway/services/rest/v1/license"
+                Method = "GET"
+                ContentType = "application/json"
+                ErrorVariable = "iwrError"
+            }
+    
+            if($body.count -ge 1){
+                $iwrArgs.add("Body",($body | ConvertTo-Json))
+            }
+    
+            if($PSVersionTable.PSVersion.Major -lt 6){
+                $basicAuth = Initialize-BasicAuthHeader -credential $Credential
+                $iwrArgs.Add("headers",$basicAuth)
+            }
+            else{
+                $iwrArgs.add("Authentication","Basic")
+                $iwrArgs.add("Credential",$Credential)
+                $iwrArgs.add("SslProtocol","Tls12")
+    
+                if($SkipCertificateCheck){
+                    $iwrArgs.add("SkipCertificateCheck",$true)
+                }
+            }
+            
+            try{
+                $response = Invoke-WebRequest @iwrArgs
+    
+                if($response.StatusCode -in 200..204){
+                    $response.Content | ConvertFrom-Json -Depth 50
+                }
+            } 
+            catch{
+                if($null -eq $iwrError){
+                    Write-Error -Message "API Call Failed"
+                }
+                else{
+                    Write-Error -Message $iwrError.Message
+                }
+            }
+        }
+                    
+    }
+    
